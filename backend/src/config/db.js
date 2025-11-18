8702df86634ec643da0affd1bb4052d2d7a0d907
Paridhi <singhalparidhi224@gmail.com>
2025-11-18 23:19:35 +0530

    fixed some code

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,11 +6,20 @@ dotenv.config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     logging: false,
+    dialectOptions: {
+      connectTimeout: 20000,
+    },
+    pool: {
+      max: 1,
+      min: 0,
+      idle: 10000,
+    }
   }
 );
 
